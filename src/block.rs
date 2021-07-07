d882c304f8c0515ad8afce3fe6f48104705ddb41
Cryptopragmatist <tristan.alberio@gmail.com>
2021-07-07 20:32:02 +0800

    struct

---
+++ src/block.rs
@@ -0,0 +1,22 @@
+use std::fmt::{ self, Debug, Formatter };
+use super::*;
+
+pub struct Block {
+    pub index: u32,
+    pub timestamp: u128,
+    pub hash: BlockHash,
+    pub prev_block_hash: BlockHash,
+    pub nonce:u64,
+    pub payload: ,
+
+
+}
+
+impl Debug for Block {
+    fn fmt (&self, f: &mut Formatter) -> fmt::Result {
+        write!(f, "Block")
+    }
+}
+
+impl Block {
+}
