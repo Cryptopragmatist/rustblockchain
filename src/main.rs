e0d0eabd3b9d37e093199006d27cf13de45fc392
Cryptopragmatist <tristan.alberio@gmail.com>
2021-07-10 11:58:31 +0800

    added mining function

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,21 @@
 use rustblockchain::*;
 
 fn main() {
-    let mut block = Block::new(0, 0, vec![0;32], 0, "Genesis Block".to_owned());
+    let mut block = Block::new(0, 0, vec![0;32], 0, "Genesis Block".to_owned(), 0x0000ffffffffffffffffffffffffffff);
+
+    block.hash = block.hash();
     
     println!( "{:?}", &block);
 
-    let h = block.hash();
-    
-    println!( "{:?}", &h);
+    block.mine();
 
-    block.hash = h;
     println!( "{:?}", &block);
+
+   // let h = block.hash();
+    
+   // println!( "{:?}", &h);
+
+   // block.hash = h;
+   // println!( "{:?}", &block);
 
 }
