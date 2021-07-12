0c63150eb0f8a290091b77549650c7d596db3e6f
Noderunner.eth <tristan.alberio@gmail.com>
2021-07-12 11:32:55 +0800

    error problem

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,35 @@
 use rustblockchain::*;
 
 fn main() {
-    let mut block = Block::new(0, 0, vec![0;32], 0, "Genesis Block".to_owned(), 0x0000ffffffffffffffffffffffffffff);
-
-    block.hash = block.hash();
+    let difficulty = 0x000fffffffffffffffffffffffffffff;
     
-    println!( "{:?}", &block);
+    let mut block = Block::new(0, 0, vec![0;32], 0, "Genesis Block".to_owned(), difficulty);
+
+    //block.hash = block.hash();
+    
+   // println!( "{:?}", &block);
 
     block.mine();
+    println!("Mined Genesis Block {:?}", &block);
+
+    let mut last_hash = block.hash.clone();
+
+    let mut blockchain = Blockchain {
+        blocks:vec![block],
+    
+    };
+
+    for i in 1..=10 {
+        let mut block = Block::new(i, 0, last_hash, 0, "Another Block".to_owned(), difficulty);
+        
+        block.mine();
+        println!("Mined Genesis Block {:?}", &block);
+
+        last_hash = block.hash.clone();
+        
+        blockchain.blocks.push(block);
+
+    }
 
     println!( "{:?}", &block);
 
